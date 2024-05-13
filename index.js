bce74a0e780dc6e05923cc6acb8eee41b06c6405
Syeda Atiya Shah <165414932+atiyashah@users.noreply.github.com>
2024-05-14 02:47:25 +0500

    This is my Third Repo

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//print welcome message
+console.log(chalk.bold.rgb(284, 284, 284)(` \n  \t\t <<<==========================>>>`));
+console.log(chalk.bold.rgb(284, 284, 284)(`<<<=======>>> ${chalk.bold.hex(`#9999FF`)(`Welcome to \`Atiya Shah \` -CLI Number-Gussing-Game`)}  <<<=========>>>`));
+console.log(chalk.bold.rgb(284, 284, 284)(`\t\t <<<==============================>>>\n`));
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: chalk.yellow("Entre your guess number Number Limit from 1 to 5:\n"),
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.green.bold("congratulation ! you guess a correct number."));
+}
+else {
+    console.log(chalk.redBright("sorry, you guess a wrong number"));
+}
